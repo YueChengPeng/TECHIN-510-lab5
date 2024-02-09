b9ce0ab9de9158928abc099446b04c050d31822d
pengyuecheng <3190102522@zju.edu.cn>
2024-02-08 23:47:07 -0800

    env_variable

--- db.py
+++ db.py
@@ -6,15 +6,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-db_user = os.getenv('DB_USER')
-db_pw = os.getenv('DB_PASSWORD')
-db_host = os.getenv('DB_HOST')
-db_port = os.getenv('DB_PORT')
-db_name = os.getenv('DB_NAME')
-
-print(db_user, db_pw, db_host, db_port, db_name)
+db_user = os.getenv('POSTGRESQLCONNSTR_DB_USER')
+db_pw = os.getenv('POSTGRESQLCONNSTR_DB_PASSWORD')
+db_host = os.getenv('POSTGRESQLCONNSTR_DB_HOST')
+db_port = os.getenv('POSTGRESQLCONNSTR_DB_PORT')
+db_name = os.getenv('POSTGRESQLCONNSTR_DB_NAME')
 
 conn_str = f'postgresql://{db_user}:{db_pw}@{db_host}:{db_port}/{db_name}'
+print(conn_str)
 
 def get_db_conn():
     conn = psycopg2.connect(conn_str)
